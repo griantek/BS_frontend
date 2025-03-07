8bad8474c7df6bbff7cf27a8cfb562d73c3a016b
MRinshad <rinshadmorayur09@gmail.com>
2025-03-07 16:08:35 +0530

    update: added some dummy code for journals create page

---
+++ app/business/editor/journals/create/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import { FC } from 'react';
+
+const CreateJournalPage: FC = () => {
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold mb-4">Create New Journal</h1>
+            <div className="max-w-2xl">
+                <p>Journal creation form will go here</p>
+            </div>
+        </div>
+    );
+};
+
+export default CreateJournalPage;
